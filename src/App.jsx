3a7d14174eb2d943c92bf1b9d5077215cdee0aca
IT-WIBRC <wibrc.se.jc@gmail.com>
2026-02-09 13:57:55 +0100

    feat: better error management and loading for better user experience

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,33 @@ import { GET_OPTIONS } from "./api/config";
 import { NETLIFY_FUNCTIONS_BASE_URL } from "./api/constant";
 import { getTrendingMovies, processGettingMovies } from "./api/movies";
 import { TrendingMovie } from "./components/TrendingMovie";
+import NoData from "./components/NoData";
+import { ToastContainer, toast } from "react-toastify";
+import MovieCardSkeleton from "./components/skeletons/MovieCardSkeleton";
+import TrendingSkeleton from "./components/skeletons/TrendingSkeleton";
 
 function App() {
   const [searchItem, setSearchItem] = useState("");
+
   const [trendingMovies, setTrendingMovies] = useState([]);
   const [movieList, setMovieList] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
+
   const [isMovieListLoading, setIsMovieListLoading] = useState(false);
+  const [areTrendingMoviesLoading, setAreTrendingMoviesLoading] =
+    useState(false);
+
   const debouncedSearchTerm = useDebounce(searchItem, 1000);
 
   useEffect(() => {
     const controller = new AbortController();
     const fetchMovies = async (query = "") => {
       setIsMovieListLoading(true);
-      setErrorMessage("");
       try {
         const movies = await processGettingMovies(query, controller.signal);
         setMovieList(() => movies ?? []);
       } catch (error) {
         if (error.name === "AbortError") return;
-        setErrorMessage("Error while fetching movies. Please try again");
+        toast.error("Error while fetching movies. Please try again");
         console.error("Error while fetching movies: ", error);
       } finally {
         setIsMovieListLoading(false);
@@ -42,12 +49,15 @@ function App() {
     const controllerTrending = new AbortController();
     const loadTrendingMovies = async () => {
       try {
+        setAreTrendingMoviesLoading(true);
         const movies = await getTrendingMovies(controllerTrending.signal);
         setTrendingMovies(movies);
       } catch (error) {
         if (error.name === "AbortError") return;
+        toast.error("Impossible to find they list of movies. Please try again");
         console.error(error);
-        // TODO: Manage this error too
+      } finally {
+        setAreTrendingMoviesLoading(false);
       }
     };
 
@@ -69,9 +79,17 @@ function App() {
           <Search searchItem={searchItem} setSearchItem={setSearchItem} />
         </header>
 
-        {trendingMovies.length > 0 && (
-          <section className="trending">
-            <h2>Trending</h2>
+        <section className="trending">
+          <h2>Trending</h2>
+          {!areTrendingMoviesLoading && trendingMovies.length === 0 ? (
+            <NoData text="No trending movies yet" />
+          ) : areTrendingMoviesLoading ? (
+            <ul className="mt-10">
+              {[1, 2, 3, 4].map((n) => (
+                <TrendingSkeleton key={n} />
+              ))}
+            </ul>
+          ) : (
             <ul>
               {trendingMovies.map(({ movie_id, poster_url }, index) => (
                 <TrendingMovie
@@ -81,26 +99,31 @@ function App() {
                 />
               ))}
             </ul>
-          </section>
-        )}
+          )}
+        </section>
 
         <section className="all-movies grow h-full flex flex-col">
           <h2 className="mt-10">All movies</h2>
-          {isMovieListLoading ? (
-            <div className="flex justify-center items-center grow h-full">
-              <Spinner />
-            </div>
-          ) : errorMessage ? (
-            <p className="text-red-500">{errorMessage}</p>
+          {!isMovieListLoading && movieList.length === 0 ? (
+            <NoData text="No movies found" />
           ) : (
             <ul>
-              {movieList.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
+              {isMovieListLoading
+                ? [1, 2, 3, 4].map((n) => <MovieCardSkeleton key={n} />)
+                : movieList.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                  ))}
             </ul>
           )}
         </section>
       </div>
+      <ToastContainer
+        position="top-right"
+        closeOnClick
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </main>
   );
 }
---
+++ src/components/NoData.jsx
@@ -0,0 +1,9 @@
+function NoData({ text }) {
+  return (
+    <div className="text-gray-300 text-lg xl:text-xl 2xl:text-2xl py-5 text-center font-bold italic">
+      {text}
+    </div>
+  );
+}
+
+export default NoData;
